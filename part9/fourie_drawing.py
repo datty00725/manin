440d7aa116e6e4de0a03cdb5aa2c6d1442b17da8
datty00725 <thekoukinn@yahoo.co.jp>
2024-05-15 16:43:08 +0900

    commit

--- part9/fourie_drawing.py
+++ part9/fourie_drawing.py
@@ -96,8 +96,51 @@ class Epicycle(mn.VMobject):
         return Draw(self)
 
 class EpicCycle(mn.Scene):
-    path_str = "M 2.614456,0.04305907 C -0.03519642,-0.05013041 0.29631932,2.7704015 0.38598443,2.8585255 L 1.2958347,1.8249996 1.1244138,1.2725989 1.2958347,1.8249996 2.2979871,0.55982315 1.7046078,1.3795156 2.1397537,1.3438767 1.7046078,1.3795156 -0.00960316,3.482204 0.1090739,3.678218 0.5969637,3.0010812 c 0.0566946,0.085227 -0.0821144,-0.036163 0,0 1.896299,0.8350973 2.1009769,-2.94190352 2.0174923,-2.95802213 z"
+    #path_str = "M 2.614456,0.04305907 C -0.03519642,-0.05013041 0.29631932,2.7704015 0.38598443,2.8585255 L 1.2958347,1.8249996 1.1244138,1.2725989 1.2958347,1.8249996 2.2979871,0.55982315 1.7046078,1.3795156 2.1397537,1.3438767 1.7046078,1.3795156 -0.00960316,3.482204 0.1090739,3.678218 0.5969637,3.0010812 c 0.0566946,0.085227 -0.0821144,-0.036163 0,0 1.896299,0.8350973 2.1009769,-2.94190352 2.0174923,-2.95802213 z"
+    coordinates2 = [
+    (193, 48), (232, 110), (245, 149), (234, 188), (205, 212), (195, 200), (213, 171),
+    (218, 148), (212, 128), (202, 97), (151, 258), (133, 281), (115, 294), (85, 291),
+    (65, 278), (53, 260), (50, 238), (58, 210), (73, 195), (97, 189), (126, 192), (143, 201)
+    ]
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        # 中心点を計算
+        num_points = len(self.coordinates2)
+        sum_x = sum(x for x, y in self.coordinates2)
+        sum_y = sum(y for x, y in self.coordinates2)
+        self.center_x = sum_x / num_points
+        self.center_y = sum_y / num_points
+
+        # 各点を中心からの相対座標に変換
+        relative_coordinates2 = [(x - self.center_x, y - self.center_y) for x, y in self.coordinates2]
+
+        # 座標範囲を計算
+        relative_coordinates2_np = np.array(relative_coordinates2)
+        self.min_x, self.max_x = relative_coordinates2_np[:, 0].min(), relative_coordinates2_np[:, 0].max()
+        self.min_y, self.max_y = relative_coordinates2_np[:, 1].min(), relative_coordinates2_np[:, 1].max()
+
+        # 新しい範囲を設定
+        self.new_min_x, self.new_max_x = -2, 2
+        self.new_min_y, self.new_max_y = -2, 2
+
+        # スケーリング関数
+        def scale(value, old_min, old_max, new_min, new_max):
+            return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
+
+        # 相対座標を新しい範囲にスケール
+        self.scaled_coordinates2 = [
+            (scale(x, self.min_x, self.max_x, self.new_min_x, self.new_max_x), 
+             scale(y, self.min_y, self.max_y, self.new_min_y, self.new_max_y))
+            for x, y in relative_coordinates2
+        ]
+
+        # パスデータを生成
+        self.path_str = "M " + " L ".join(f"{x},{y}" for x, y in self.scaled_coordinates2) + " Z"
+
+    
+        
     def construct(self):
         # Scale because I don't want to edit the SVG because I'm lazy
         path = SVGPath(self.path_str).scale(1.75)
@@ -105,15 +148,21 @@ class EpicCycle(mn.Scene):
 
         self.add(path)
 
-        for num_coeffs in range(5, 105 + 1, 20):
+        for num_coeffs in range(2, 105 + 1, 260):
             ep = Epicycle(path, num_coeffs)
             ep_copy = ep.copy().draw_circles(0, color=mn.BLUE)
 
             text = mn.Tex(f"Cycles: {num_coeffs}").to_edge(mn.UP).shift(mn.LEFT)
 
             self.play(mn.FadeIn(mn.VGroup(ep_copy, text)), run_time=0.75)
+            self.wait(1)
+            self.play(mn.FadeOut(ep_copy))
             self.remove(ep_copy)
-
+            
+            # Epicycleオブジェクトをリセット
+            ep.clear_points()
+            ep.submobjects.clear()
+            
             self.play(ep.animate_circles(color=mn.BLUE), run_time=10, rate_func=mn.linear)
 
             self.play(mn.FadeOut(mn.VGroup(ep, text)))
